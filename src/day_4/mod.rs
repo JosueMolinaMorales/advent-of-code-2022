dc130a8d5a1051d60c054e4fce6ddd985a141d89
Josue Morales <molinajosue92@hotmail.com>
2022-12-04 09:13:28 -0500

    finish day 4

---
+++ src/day_4/mod.rs
@@ -0,0 +1,52 @@
+use std::fs;
+
+
+const INPUT_FILE: &str = "./inputs/day_4_input.txt";
+
+pub fn solve_day_four() {
+    let input: Vec<Vec<u32>> = fs::read_to_string(INPUT_FILE).unwrap()
+        .split("\n")
+        .map(|line| {
+            line.replace("-", ",").split(",").map(|string| string.parse::<u32>().unwrap()).collect::<Vec<_>>()
+        })
+        .collect();
+    part_one(input.clone());
+    part_two(input.clone());
+}
+
+fn part_two(input: Vec<Vec<u32>>) {
+    let res: u32 = input.iter().map(|pairs| {
+        let left = (pairs[0], pairs[1]);
+        let right = (pairs[2], pairs[3]);
+
+        if right.0 >= left.0 && right.0 <= left.1 || left.0 >= right.0 && left.0 <= right.1 {
+            return 1;
+        }
+
+        0
+    }).sum();
+    println!("Part 2 result: {}", res);
+}
+
+fn part_one(input: Vec<Vec<u32>>) {
+    let res: u32 = input.iter()
+        .map(|pairs| {
+            let left = (pairs[0], pairs[1]);
+            let right = (pairs[2], pairs[3]);
+
+            // Check to see if right endpoint one is between left endpoints and
+            // Check to see if right endpoint two is also in between left endpoints
+            if 
+                (right.0 >= left.0 && right.0 <= left.1 &&
+                right.1 >= left.0 && right.1 <= left.1) || 
+                (left.0 >= right.0 && left.0 <= right.1 &&
+                left.1 >= right.0 && left.1 <= right.1)
+            {
+                // Right pair is in between left pair or left pair is in between right pair
+                return 1
+            }
+            0
+        }).sum();
+
+    println!("Part 1 result is: {}", res);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 pub mod day_1;
 pub mod day_2;
 pub mod day_3;
+pub mod day_4;
 
 fn main() {
     // day_1::solve_day_one()
     // day_2::solve_day_two()
-    day_3::solve_day_3()
+    // day_3::solve_day_3()
+    day_4::solve_day_four()
 }
