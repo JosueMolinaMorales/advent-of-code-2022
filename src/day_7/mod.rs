3491a9e6d91935d2a152318e66f6896e764a2829
Josue Morales <molinajosue92@hotmail.com>
2022-12-07 17:40:25 -0500

    almost finished

--- src/day_7/mod.rs
+++ src/day_7/mod.rs
@@ -1,146 +1,155 @@
-use std::{fs, collections::BTreeMap};
+use std::{fs, collections::{BTreeMap, HashMap}};
 
 const TEST_INPUT: &str = "./input7.txt";
 const INPUT_FILE: &str = "./inputs/day_7_input.txt";
 
-#[derive(Debug)]
-struct File {
-    size: u32,
-    name: String
+pub fn solve_day_seven() {
+    let input = fs::read_to_string(INPUT_FILE).unwrap();
+    part_one(input);
 }
 
 #[derive(Debug)]
 struct Directory {
     name: String,
-    size: Option<u32>,
-    parent: Option<Box<Directory>>,
-    files: Option<Vec<File>>
-}
-
-#[derive(Debug)]
-enum Commands {
-    CD,
-    LS
-}
-
-pub fn solve_day_seven() {
-    let input = fs::read_to_string(TEST_INPUT).unwrap();
-    part_one(input);
+    size: u32,
+    parent: Option<String>,
+    children: Vec<String>
 }
 
 fn part_one(input: String) {
-    let mut current_dir = "".to_string();
-    let mut last_command: Commands = Commands::CD;
-    let mut directory_structure: BTreeMap<String, Directory> = BTreeMap::new(); // String will be name of directory
-    let mut stack: Vec<String> = Vec::new(); // Stores the history of directories. Used for ..
+    let mut directory_structure: HashMap<String, Directory> = HashMap::new();
+    let mut stack: Vec<String> = Vec::new(); // Stores the history of directories. 
 
-    for output in input.split("\n") {
-        match output.chars().nth(0).unwrap() {
+    for line in input.split("\n") {
+        match line.chars().nth(0).unwrap() {
             '$' => {
-                // Executable action
-                let parse = output.split(" ").map(|f| f.trim()).collect::<Vec<&str>>();
-                match parse[1] {
-                    "cd" => {
-                        last_command = Commands::CD;
-                        // CD options are -> .. or {name}
-                        match parse[2] {
-                            ".." => {
-                                // Go back
-                                stack.pop();
-                                current_dir = stack.last().unwrap().to_owned();
-                            },
-                            dir_name => {
-                                if current_dir == "" {
-                                    // previous directory
-                                    // Inserts the directory into the directory structure, if it is already in there, None is returned
-                                    directory_structure.insert(dir_name.to_string(), Directory { 
-                                        name: dir_name.to_string(), 
-                                        size: None, 
-                                        sub_directories: None, 
-                                        files: None 
-                                    });
-                                } else {
-                                    // Inside a current directory, loop through stack and get the Directory Object
-                                    for dir_name_from_stack in stack.iter() {
-                                        let dir = directory_structure.get_mut(dir_name_from_stack).unwrap();
-                                        if dir.name == current_dir {
-                                            // Within the current dir, add the directory to 
-                                            match dir.sub_directories.as_mut() {
-                                                None => dir.sub_directories = Some(vec![Box::new(Directory{ name: dir_name.to_string(), size: None, sub_directories: None, files: None})]),
-                                                Some(sub_dirs) => {
-                                                    if sub_dirs.iter().find(|d| d.name == dir_name.to_string()).is_none() {
-                                                        // Directory not in subdir list, add it
-                                                        sub_dirs.push(Box::new(Directory { name: dir_name.to_string(), size: None, sub_directories: None, files: None}))
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                                // Insert the directory into the stack
-                                stack.push(dir_name.to_string());
-                                // Set the current directory
-                                current_dir = dir_name.to_string();
-                            }
-                        }
-                    },
-                    "ls" => {
-                        last_command = Commands::LS
-                    },
-                    _ => unreachable!()
+                let exec = line.split(" ").map(|s| s.trim()).collect::<Vec<&str>>();
+                if exec[1] != "cd" {
+                    continue
                 }
+                let dir_name = exec[2].to_string();
+                if dir_name == ".." {
+                    stack.pop();
+                    continue;
+                }
+
+                // Check to see if the dir name is already in struct
+                let (parent, parent_size) = match directory_structure.get(stack.last().unwrap_or(&"".to_string())) {
+                    Some(parent) => (Some(parent.name.clone()), parent.size),
+                    None => (None, 0)
+                };
+
+                directory_structure
+                .entry(dir_name.clone())
+                .or_insert(Directory { 
+                    name: dir_name.clone(),
+                    size: parent_size, 
+                    parent,
+                    children: Vec::new()
+                });
+
+                stack.push(dir_name);
             },
             _ => {
-                // Line is ls output
-                let output_line = output.split(" ").collect::<Vec<&str>>();
-                match output_line[0] {
+                // ls output
+                let output = line.split(" ").map(|s| s.trim()).collect::<Vec<&str>>();
+                match output[0] {
                     "dir" => {
-                        // This is a directory
-                        let name_of_dir = output_line[1].trim();
-                        for dir_name in stack.iter() {
-                            println!("{}", dir_name);
-                            let dir = directory_structure.get_mut(dir_name).unwrap();
-                            if dir.name == current_dir {
-                                // Within the current dir, add the directory to 
-                                match dir.sub_directories.as_mut() {
-                                    None => dir.sub_directories = Some(vec![Box::new(Directory{ name: name_of_dir.to_string(), size: None, sub_directories: None, files: None})]),
-                                    Some(sub_dirs) => {
-                                        if sub_dirs.iter().find(|d| d.name == name_of_dir.to_string()).is_none() {
-                                            // Directory not in subdir list, add it
-                                            sub_dirs.push(Box::new(Directory { name: name_of_dir.to_string(), size: None, sub_directories: None, files: None}))
-                                        }
-                                    }
-                                }
+                        let dir_name = output[1].to_string();
+                        directory_structure
+                        .entry(dir_name.clone())
+                        .and_modify(|dir| {
+                            match dir.parent {
+                                None => {
+                                    dir.parent = Some(stack.last().unwrap().clone())
+                                },
+                                Some(_) => {}
                             }
-                        }
+                        })
+                        .or_insert(Directory {
+                            name: dir_name.clone(),
+                            size: 0,
+                            parent: Some(stack.last().unwrap().clone()),
+                            children: Vec::new()
+                        });
+
+                        let curr_dir = stack.last().unwrap().clone();
+                        // add dir as a child of the current directory
+                        directory_structure
+                            .entry(curr_dir.clone())
+                            .and_modify(|dir| {
+                                dir.children.push(dir_name.clone());
+                            });
+                        
                     },
                     size => {
-                        // This is a file
-                        for dir_name in stack.iter() {
-                            let dir = directory_structure.get_mut(dir_name).unwrap();
-                            if dir.name == current_dir {
-                                // Within the current dir, add the file to directory
-                                let file_name = output_line[1].to_string();
-                                match dir.files.as_mut() {
-                                    None => {
-                                        dir.files = Some(vec![File{name: file_name, size: size.parse().unwrap()}])
-                                    },
-                                    Some(files) => {
-                                        // Files exists
-                                        if files.iter().find(|f| f.name == file_name).is_none() {
-                                            // file does not exist in file vec
-                                            files.push(File { size: size.parse().unwrap(), name: file_name})
-                                        }
-                                    }
-                                }
-                            }
+                        let curr_dir = stack.last().unwrap().clone();
+                        let size = size.parse::<u32>().unwrap(); 
+                        directory_structure
+                            .entry(curr_dir.to_string())
+                            .and_modify(|dir| {
+                                dir.size += size;
+                            });
+                        let mut parent_dir = directory_structure.get(&curr_dir).unwrap().parent.clone();
+                        while let Some(parent) = parent_dir.clone() {
+                            directory_structure.entry(parent.clone())
+                            .and_modify(|dir| {
+                                dir.size += size;
+                                parent_dir = dir.parent.clone(); 
+                            });
                         }
                     }
                 }
-                
             }
         }
     }
 
-    println!("After loop: {:#?}", directory_structure);
+
+    let res = directory_structure.iter()
+    .map(|dir| {
+        dir.1.size
+    })
+    .filter(|size| *size < 100000)
+    .sum::<u32>();
+    println!("part 1 res: {}", res);
 }
+
+// #[test]
+// fn test_find_sub_dir() {
+//     let mut sub_dir_tree = BTreeMap::new();
+//     sub_dir_tree.insert("D".to_string(), Box::new(Directory {
+//         name: "D".to_string(),
+//         size: None,
+//         sub_directories: BTreeMap::new(),
+//         files: Vec::new(),
+//     }));
+//     let mut dir_tree = BTreeMap::new();
+//     dir_tree.insert("A".to_string(), Box::new(Directory {
+//         name: "A".to_string(),
+//         size: None,
+//         sub_directories: BTreeMap::new(),
+//         files: Vec::new()
+//     }));
+//     dir_tree.insert("B".to_string(), Box::new(Directory {
+//         name: "B".to_string(),
+//         size: None,
+//         sub_directories: BTreeMap::new(),
+//         files: Vec::new()
+//     }));
+//     dir_tree.insert("C".to_string(),Box::new(Directory {
+//         name: "C".to_string(),
+//         size: None,
+//         sub_directories: sub_dir_tree,
+//         files: Vec::new()
+//     }));
+//     let dir_struct = Directory {
+//         name: "/".to_string(),
+//         size: None,
+//         sub_directories: dir_tree,
+//         files: Vec::new()
+//     };
+
+//     let found = dir_struct.find_subdirectory("D".to_string());
+//     assert!(found.is_some());
+//     assert_eq!(found.unwrap().name, "D".to_string())
+// }
