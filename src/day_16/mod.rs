f1df2f471769d4be86090b4d8a0e1609fea4f770
Josue Morales <molinajosue92@hotmail.com>
2022-12-17 09:36:50 -0500

    updates for pat 2

--- src/day_16/mod.rs
+++ src/day_16/mod.rs
@@ -1,14 +1,13 @@
 use std::collections::{VecDeque, HashMap};
 
-const TEST_INPUT: &str = include_str!("input_day_16.txt");
-
+const TEST_INPUT: &str = include_str!("test_input.txt");
+const INPUT: &str = include_str!("input_day_16.txt");
 #[derive(Debug, Clone)]
 struct Tunnel {
     pub valve_name: String,
     pub rate: i32,
     pub adj_tunnels: Vec<String>,
     pub is_opened: bool,
-    pub parent: Option<String>,
 }
 
 fn parse_input(input: &str) -> HashMap<String, Tunnel> {
@@ -17,26 +16,86 @@ fn parse_input(input: &str) -> HashMap<String, Tunnel> {
         .map(|line| {
             let line = line.replace("Valve ", "").replace("has flow rate=", "").replace("; tunnels lead to valves ", " ").replace("; tunnel leads to valve ", " ").replace(", ", ",");
             let line = line.split(" ").collect::<Vec<&str>>();
-            // println!("{}", line);
             let tunnel_id = line[0].clone();
             ( tunnel_id.clone().to_string(), Tunnel {
                 valve_name: tunnel_id.clone().to_string(),
                 rate: line[1].clone().parse::<i32>().unwrap(),
                 adj_tunnels: line[2].clone().split(",").map(|s| s.to_string()).collect::<Vec<String>>(),
                 is_opened: false,
-                parent: None,
             })
         })
         .collect::<HashMap<String, Tunnel>>()
 }
 
 pub fn solve_day_16() {
-    let mut tunnels = parse_input(TEST_INPUT);
-    solve_part_one(&mut tunnels, "AA".to_string());
+    let tunnels = parse_input(INPUT);
+
+    solve_part_one(&mut tunnels.clone(), "AA".to_string());
+    // solve_part_two(&mut tunnels.clone());
+}
+
+fn solve_part_two(tunnels: &mut HashMap<String, Tunnel>) {
+    println!("final res: {}", bfs_part_two(tunnels, "AA".to_string(), 0, 26));
+
 }
 
 fn solve_part_one(tunnels: &mut HashMap<String, Tunnel>, starting: String) {
     println!("final res: {}", bfs(tunnels, starting, 0, 30));
+}
+
+fn bfs_part_two(tunnels: &mut HashMap<String, Tunnel>, starting: String, existing_calc: usize, time_remaining: usize) -> usize {
+    // Base case, run out of time
+    if time_remaining <= 0 {
+        return existing_calc
+    }
+    // Set up for bfs
+    let mut queue: VecDeque<String> = VecDeque::new();
+    let mut explored: Vec<String> = vec![starting.clone()];
+    queue.push_back(tunnels.get(&starting).unwrap().valve_name.clone());
+
+    let mut distance: HashMap<String, usize> = HashMap::new(); // Hashmap of all distances with respect to starting point
+    distance.insert(starting.to_string(), 0);
+
+    let mut calculations = Vec::new();
+    while !queue.is_empty() {
+        let v = queue.pop_front().unwrap();
+        let v_dist = distance.get(&v).unwrap().clone();
+
+        let adj = tunnels.get(&v).unwrap().adj_tunnels.clone();
+        for w in adj {
+            if !explored.contains(&w) {
+                // Get the distance
+                let d = distance.entry(w.clone()).and_modify(|d| *d = v_dist + 1).or_insert(v_dist+1);
+                let rate = tunnels.get(&w).unwrap().rate as isize;
+                // Calculate the Time * Rate
+                let calc = ((time_remaining as isize - *d as isize - 1) as isize) * rate;
+                let t = tunnels.get(&w).unwrap();
+                if calc > 0 && !t.is_opened {
+                    calculations.push((w.clone(), calc as usize , time_remaining-*d-1));
+                }
+                explored.push(w.clone());
+                queue.push_back(w);
+            }
+        }
+    }
+    calculations.sort_by(|a, b| a.1.cmp(&b.1));
+    if calculations.len() == 0 {
+        return existing_calc
+    }
+
+    let res = calculations.iter().rev().take(2).collect::<Vec<&(String, usize, usize)>>();
+    // let mut tunnels = tunnels.clone();
+    let elaphent = res.last().unwrap().clone();
+    let person = res.first().unwrap().clone();
+    println!("Starting point is: {} and the person has: {:?} and elaphent has {:?}", starting, person, elaphent);
+    let mut tunnels = tunnels.clone();
+    tunnels.entry(elaphent.0.to_string()).and_modify(|t| t.is_opened = true);
+    let elaphent = bfs_part_two(&mut tunnels, elaphent.0.clone(), elaphent.1, elaphent.2);
+    tunnels.entry(person.0.to_string()).and_modify(|t| t.is_opened = true);
+    let person = bfs_part_two(&mut tunnels, person.0.clone(), person.1, person.2);
+
+    person.max(elaphent) + existing_calc
+    
 }
 
 fn bfs(tunnels: &mut HashMap<String, Tunnel>, starting: String, existing_calc: usize, time_remaining: usize) -> usize {
