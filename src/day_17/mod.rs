209fb4b62b35405d8f44f8848d06ec73f70fbd93
Josue Morales <molinajosue92@hotmail.com>
2022-12-18 12:59:36 -0500

    working on day 17

--- src/day_17/mod.rs
+++ src/day_17/mod.rs
@@ -1,5 +1,6 @@
 
 const TEST_INPUT: &str = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>";
+const INPUT: &str = include_str!("day_17_input.txt");
 const LINE: [&str; 4] = ["#", "#", "#", "#"];
 const PLUS: [[&str; 3]; 3] = [
     [".", "#", "."],
@@ -75,28 +76,56 @@ pub fn solve_day_17() {
     let mut gases_iter = TEST_INPUT.chars().into_iter().cycle();
     let rocks = vec![Rocks::HorizontalLine, Rocks::Plus, Rocks::LShape, Rocks::VerticalLine, Rocks::Square];
     let mut rocks_iter = rocks.iter().cycle();
-    while rock_count <= 4 {
+    while rock_count < 2022 {
         // Start falling down loop
         // First get the height of the grid, this is where the rock will start
         let mut grid_height = grid.len();
         // Current rock falling
         let curr_rock = rocks_iter.next().unwrap();
         let mut width_range: (usize, usize) = (2, 1+curr_rock.get_width());
+        // print_grid(&grid);
         'falling: loop {
             // Then jet moves the rock
             let jet_move = gases_iter.next().unwrap();
-            println!("width range for {:?} is: {:?} and jet move is: {} and grid height is currently: {}", curr_rock, width_range, jet_move, grid_height);
+            // println!("width range for {:?} is: {:?} and jet move is: {} and grid height is currently: {}", curr_rock, width_range, jet_move, grid_height);
             match jet_move {
                 '>' => {
+                    //. Checks for right wall
                     if width_range.1 < 6 {
-                        width_range.1 += 1;
-                        width_range.0 += 1;
+                        let mut move_rock = true;
+                        // Check to see if it will run into an already existing rock
+                        let rock_height = curr_rock.get_height();
+                        for i in 0..rock_height {
+                            if (i + grid_height) >= grid.len() {
+                                break;
+                            }
+                            if grid[i+grid_height][width_range.1] == "#" || grid[i+grid_height][width_range.1 + 1] == "#" {
+                                move_rock = false
+                            }
+                        }
+                        if move_rock {
+                            width_range.1 += 1;
+                            width_range.0 += 1;
+                        }
                     }
                 },
                 '<' => {
                     if width_range.0 > 0 {
-                        width_range.0 -= 1;
-                        width_range.1 -= 1;
+                        let mut move_rock = true;
+                        // Check to see if it will run into an already existing rock
+                        let rock_height = curr_rock.get_height();
+                        for i in 0..rock_height {
+                            if (i + grid_height) >= grid.len() {
+                                break;
+                            }
+                            if grid[i+grid_height][width_range.0] == "#" || grid[i+grid_height][width_range.0 - 1] == "#" {
+                                move_rock = false
+                            }
+                        }
+                        if move_rock {
+                            width_range.0 -= 1;
+                            width_range.1 -= 1;
+                        }
                     }
                 },
                 _ => unreachable!()
@@ -106,13 +135,6 @@ pub fn solve_day_17() {
             // If there is, leave rock there and break out of loop
             for r in width_range.0..=width_range.1 {
                 if grid_height == 0 || grid[grid_height-1][r] == "#" {
-                    // Place rock
-                    // println!("grid_height: {}, width range: {:?}", grid_height, width_range);
-                    // for i in curr_rock.get_rock_shape() {
-                    //     for c in i.iter().enumerate() {
-                    //         grid[width_range.0][c.0] = *c.1;
-                    //     }
-                    // }
                     break 'falling;
                 }
             }
@@ -120,35 +142,41 @@ pub fn solve_day_17() {
             // else rock falls down
             grid_height -= 1;
         } 
+
         for (i, cols) in curr_rock.get_rock_shape().iter().enumerate() {
             for c in cols.iter().enumerate() {
+                // println!("current rock: {:?}, i: {}, c.0: {}, grid_height: {}, grid.len(): {}", curr_rock, i, c.0, grid_height, grid.len());
+                if grid_height + i >= grid.len() {
+                    for _ in 0..=((grid_height+i) - grid.len()) {
+                        grid.push([".",".",".",".",".",".",".",].to_vec())
+                    }
+                }
                 grid[grid_height + i][width_range.0 + c.0] = *c.1;
             }
         }
-        // Add three empty spaces for next fall
-       
         // Add three empty Spaces above ground or highest rock
         let height = get_height_of_rocks(&grid);
-        println!("get_height_of_rocks: {}", height);
-        for _ in height..height+1 {
-            grid.push([".",".",".",".",".",".",".",].to_vec());
+        for i in 1..=3 {
+            if height + i >= grid.len() {
+                grid.push([".",".",".",".",".",".",".",].to_vec());
+            } 
         }
-        print_grid(&grid);
-        println!("\n\n");
         // Rock has fallen, increase rock_count for next iteration
         rock_count += 1;
     }
+    print_grid(&grid);
+    println!("Height: {}", get_height_of_rocks(&grid));
+    println!("Height of grid: {}", grid.len());
 }
 
 fn get_height_of_rocks(grid: &Vec<Vec<&str>>) -> usize {
     for (i, row) in grid.iter().enumerate() {
-        println!("{} -> {:?}", i, row);
-        if row.iter().any(|x| *x == ".") && i != 0{
+        if row.iter().all(|x| *x == ".") {
             // if the entire row contains '.' then the last row had at least one '#'
             return i - 1
         }
     }
-    0
+    grid.len() - 1
 }
 
 fn print_grid(grid: &Vec<Vec<&str>>) {
